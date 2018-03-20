7aac94799e99c377000dbe987872ebe8a32e1663
ARThe <dawidkwapisz@MacBook-Pro-Dawid.local>
2018-03-20 21:15:41 +0100

    Ad reCaptcha and others changes with multi auth

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -18,17 +18,39 @@ class AdminLoginController extends Controller
         return view('admin.admin-login');
     }
 
+    /**
+     * Check is admin correct login
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function login(Request $request)
     {
         $this->validate($request, [
             'email' => 'required|email',
-            'password' => 'required|min:6'
+            'password' => 'required|min:6',
+            'g-recaptcha-response' => 'required|recaptcha',
         ]);
 
         if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])) {
             return redirect()->intended(route('admin.dashboard'));
         }
 
-        return redirect()->back()->withInput($request->only('email'));
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+    * Get the failed login response instance.
+    *
+    * @param  Request  $request
+    * @return \Illuminate\Http\RedirectResponse
+    */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $errors = new \Illuminate\Support\MessageBag();
+        $errors->add('email', 'Your custom error message!');
+        return redirect()->back()
+            ->withInput($request->only('email'))
+            ->withErrors($errors);
     }
 }
--- resources/views/Admin/admin-login.blade.php
+++ resources/views/Admin/admin-login.blade.php
@@ -39,6 +39,13 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label text-md-right"></label>
+                            <div class="col-md-6">
+                                {!! Recaptcha::render() !!}
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
